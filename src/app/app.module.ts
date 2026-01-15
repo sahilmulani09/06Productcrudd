eefedf71463d509c571053947a76fc9e5283a621
sahilmulani09 <sahilmulani462@gmail.com>
2026-01-15 19:15:34 +0530

    okktre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductDashhComponent } from './shared/component/product-dashh/product-dashh.component';
 import { ProductFormComponent } from './shared/component/product-form/product-form.component';
 import { ProductListComponent } from './shared/component/product-list/product-list.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-
+import { ProductDashhComponent } from './shared/component/product-dashh/product-dashh.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductDashhComponent,
     ProductFormComponent,
     ProductListComponent,
+    ProductDashhComponent,
 
   ],
   imports: [
